44af7e89b3ed70408c07f4fd91b783166fa31ba6
rana-ghub <ssshashankrana001@gmail.com>
2020-09-09 20:18:53 +0530

    Login and signup

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.index, name="index"),
+    path('register/', views.register, name="register"),
+    path('register/register', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    path('login/login', views.login, name="login"),
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def login(request):
+    if request.method == 'POST':    
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username= username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect (request, 'order/')
+        else:
+            messages.info(request, 'Username or password incorrect')
+            return redirect('/order/login')
+    else:
+        return render(request, 'login.html')
+
+
+
+def register(request):
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        contact_no = request.POST['number']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['confirm']
+        email = request.POST['email']
+
+        if password == password2:
+            if User.objects.filter(username = username).exists():
+                messages.info(request, 'username already taken!')
+                return redirect('/order/register')
+            elif User.objects.filter(email = email).exists():
+                messages.info(request, 'Email already taken!')
+                return redirect('/order/register')
+            else:
+                user = User.objects.create_user( first_name = first_name, username= username, password = password, email = email, last_name = last_name)
+                user.save()
+                messages.info(request, 'Account created!')
+                return redirect('order/login')
+        else:
+            messages.info(request, 'Password not matching')
+        return redirect('/order')
+    else:
+        return render(request, 'register.html',{})
